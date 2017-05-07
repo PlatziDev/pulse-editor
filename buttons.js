5c1f37372e14e68454b50bda17f90c168641b5c4
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-05-07 15:33:48 -0500

    [add] buttons exporter

---
+++ buttons.js
@@ -0,0 +1,12 @@
+exports.Base = require('./built/buttons/base.js').default
+exports.Bold = require('./built/buttons/bold.js').default
+exports.Code = require('./built/buttons/code.js').default
+exports.Heading = require('./built/buttons/heading.js').default
+exports.Image = require('./built/buttons/image.js').default
+exports.Italic = require('./built/buttons/italic.js').default
+exports.Link = require('./built/buttons/link.js').default
+exports.OrderedList = require('./built/buttons/ordered-list.js').default
+exports.Quote = require('./built/buttons/quote.js').default
+exports.Underline = require('./built/buttons/underline.js').default
+exports.UnorderedList = require('./built/buttons/unordered-list.js').default
+exports.Youtube = require('./built/buttons/youtube.js').default
