d3a0e163a2c1f9028ee1fba96441fecadfea43a0
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-05-07 15:28:16 -0500

    [add] base button component

---
+++ src/buttons/base.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const emptyFunc = () => {}
+
+/**
+ * The basic button without any functionality
+ * @param {Object} props The base button props
+ */
+function BaseButton (props) {
+  return (
+    <button
+      {...props}
+      className={props.className}
+      onClick={props.onClick}
+      name={props.name}
+      disabled={props.disabled}
+      type='button'
+      children={props.children}
+    />
+  )
+}
+
+BaseButton.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+  disabled: PropTypes.bool,
+  name: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired
+}
+
+BaseButton.defaultProps = {
+  className: 'LabEditor-button',
+  disabled: false,
+  onClick: emptyFunc
+}
+
+export default BaseButton
