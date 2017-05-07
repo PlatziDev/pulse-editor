bc339186f76c28e95e754e019cb711be028c0205
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-05-07 15:32:33 -0500

    [add] field component

---
+++ src/components/field.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Textarea from 'react-textarea-autosize'
+
+class Field extends Component {
+  static propTypes = {
+    className: PropTypes.string
+  };
+
+  static defaultProps = {
+    className: 'LabEditor-field'
+  };
+
+  static contextTypes = {
+    name: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    writeValue: PropTypes.func.isRequired,
+    syncScroll: PropTypes.func.isRequired,
+    detectShortcut: PropTypes.func.isRequired,
+    disabled: PropTypes.bool.isRequired
+  };
+
+  handleChange = this.context.writeValue;
+
+  handleScroll = this.context.syncScroll;
+
+  handleKeyDown = this.context.detectShortcut;
+
+  render () {
+    return (
+      <Textarea
+        minRows={3}
+        maxRows={10}
+        name='value'
+        {...this.props}
+        className={this.props.className}
+        disabled={this.context.disabled}
+        id={`labeditor-${this.context.name}`}
+        onChange={this.handleChange}
+        onKeyDown={this.handleKeyDown}
+        value={this.context.value}
+      />
+    )
+  }
+}
+
+export default Field
