1816cf646bf1746c7f445af6dddd102e6381a690
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-05-07 15:29:38 -0500

    [add] ordered list button component

---
+++ src/buttons/ordered-list.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import BaseButton from './base'
+
+function updater (selected, full, selection) {
+  if (selection.start === 0) {
+    return `1. ${selected}`
+  }
+  return `\n1. ${selected}`
+}
+
+function handler (event) {
+  if (event.selection.start === 0) {
+    return {
+      start: event.selection.start + 3 + event.selected.length,
+      end: event.selection.end + 3
+    }
+  }
+  return {
+    start: event.selection.start + 4 + event.selected.length,
+    end: event.selection.end + 4
+  }
+}
+
+class ButtonOrderedList extends Component {
+  static propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node.isRequired
+  };
+
+  static defaultProps = {
+    className: 'LabEditor-button'
+  };
+
+  static contextTypes = {
+    updateValue: PropTypes.func.isRequired
+  };
+
+  handleClick = event => {
+    event.preventDefault()
+    this.context.updateValue({ ...event, updater, handler })
+  };
+
+  render () {
+    return (
+      <BaseButton
+        className={this.props.className}
+        onClick={this.handleClick}
+        disabled={this.props.disabled}
+        name='ordered-list'
+      >
+        {this.props.children}
+      </BaseButton>
+    )
+  }
+}
+
+export default ButtonOrderedList
