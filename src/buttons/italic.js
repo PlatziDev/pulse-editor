0b76a90db7295cd5bc6120680bac10fc59e2557e
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-05-07 15:29:26 -0500

    [add] italic button component

---
+++ src/buttons/italic.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import BaseButton from './base'
+
+import isMac from '../utils/is-mac'
+
+function updater (selected) {
+  return `_${selected}_`
+}
+
+function handler (event) {
+  return {
+    start: 1 + event.selection.start,
+    end: event.selection.end + 1
+  }
+}
+
+class ButtonItalic extends Component {
+  static propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node.isRequired
+  };
+
+  static defaultProps = {
+    className: 'LabEditor-button'
+  };
+
+  static contextTypes = {
+    updateValue: PropTypes.func.isRequired,
+    setShortcut: PropTypes.func.isRequired
+  };
+
+  componentDidMount () {
+    this.context.setShortcut({
+      ctrlKey: !isMac(),
+      metaKey: isMac(),
+      altKey: false,
+      shiftKey: false,
+      keyName: 'i',
+      updater,
+      handler
+    })
+  }
+
+  componenWillUnmount () {
+    this.context.removeShortcut({ keyName: 'i' })
+  }
+
+  handleClick = event => {
+    event.preventDefault()
+    this.context.updateValue({ ...event, updater, handler })
+  };
+
+  render () {
+    return (
+      <BaseButton
+        className={this.props.className}
+        onClick={this.handleClick}
+        disabled={this.props.disabled}
+        name='italic'
+      >
+        {this.props.children}
+      </BaseButton>
+    )
+  }
+}
+
+export default ButtonItalic
