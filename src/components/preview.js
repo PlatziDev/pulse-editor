5561b49f8d67ba108787828d8e78f57f625a3e7f
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-05-07 15:32:39 -0500

    [add] preview component

---
+++ src/components/preview.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import Markdown from '@platzi/react-markdown'
+
+class Preview extends Component {
+  static propTypes = {
+    className: PropTypes.string,
+    parser: PropTypes.object
+  };
+
+  static defaultProps = {
+    className: 'LabEditor-preview',
+    parser: {}
+  };
+
+  static contextTypes = {
+    value: PropTypes.string.isRequired
+  };
+
+  render () {
+    return (
+      <Markdown
+        className={this.props.className}
+        content={this.context.value}
+        parser={this.props.parser}
+      />
+    )
+  }
+}
+
+export default Preview
