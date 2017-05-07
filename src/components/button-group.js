3711de053cf1006c99c7da0ab17b936a7772761b
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-05-07 15:32:14 -0500

    [add] button group component

---
+++ src/components/button-group.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function ButtonGroup ({ children, className }) {
+  return (
+    <div className={className}>
+      {children}
+    </div>
+  )
+}
+
+ButtonGroup.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+}
+
+ButtonGroup.defaultProps = {
+  className: 'LabEditor-buttonGroup'
+}
+
+export default ButtonGroup
