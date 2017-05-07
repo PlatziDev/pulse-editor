f298e031e994d2602031611dc2214ac524189dd4
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-05-07 15:32:09 -0500

    [add] button bar component

---
+++ src/components/button-bar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function ButtonBar ({ children, className }) {
+  return (
+    <div className={className}>
+      {children}
+    </div>
+  )
+}
+
+ButtonBar.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired
+}
+
+ButtonBar.defaultProps = {
+  className: 'LabEditor-buttonBar'
+}
+
+export default ButtonBar
