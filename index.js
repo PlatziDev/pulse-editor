882dae9026edd8325c3e0ded24f6a70d492890a4
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-05-07 15:33:59 -0500

    [add] core components exporter

---
+++ index.js
@@ -0,0 +1,6 @@
+exports.ButtonBar = require('./built/components/button-bar.js').default
+exports.ButtonGroup = require('./built/components/button-group.js').default
+exports.Editor = require('./built/components/editor.js').default
+exports.EmojiBar = require('./built/components/emoji-bar.js').default
+exports.Field = require('./built/components/field.js').default
+exports.Preview = require('./built/components/preview.js').default
