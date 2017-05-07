827266702429f030da1d78229e81c48f7c906865
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-05-07 15:29:52 -0500

    [add] underline button component

---
+++ src/buttons/underline.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import BaseButton from './base'
+
+import isMac from '../utils/is-mac'
+
+function updater (selected) {
+  return `++${selected}++`
+}
+
+function handler (event) {
+  return {
+    start: 2 + event.selection.start,
+    end: event.selection.end + 2
+  }
+}
+
+class ButtonUnderline extends Component {
+  static propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node.isRequired
+  };
+
+  static defaultProps = {
+    className: 'LabEditor-button'
+  };
+
+  static contextTypes = {
+    updateValue: PropTypes.func.isRequired,
+    setShortcut: PropTypes.func.isRequired,
+    removeShortcut: PropTypes.func.isRequired
+  };
+
+  componentDidMount () {
+    this.context.setShortcut({
+      ctrlKey: !isMac(),
+      metaKey: isMac(),
+      altKey: false,
+      shiftKey: false,
+      keyName: 'u',
+      updater,
+      handler
+    })
+  }
+
+  componenWillUnmount () {
+    this.context.removeShortcut({ keyName: 'u' })
+  }
+
+  handleClick = event => {
+    event.preventDefault()
+    this.context.updateValue({ ...event, updater, handler })
+  };
+
+  render () {
+    return (
+      <BaseButton
+        className={this.props.className}
+        onClick={this.handleClick}
+        disabled={this.props.disabled}
+        name='underline'
+      >
+        {this.props.children}
+      </BaseButton>
+    )
+  }
+}
+
+export default ButtonUnderline
