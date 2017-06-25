a92a468157915371f1b53e2fe41e368cd2b72fac
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-06-25 04:05:52 -0500

    Add file to load editor utils

---
+++ utils.js
@@ -0,0 +1,7 @@
+exports.createChangeEvent = require('./built/utils/create-change-event.js').default
+exports.getKeyName = require('./built/utils/get-key-name.js').default
+exports.getSelected = require('./built/utils/get-selected.js').default
+exports.getSelection = require('./built/utils/get-selection.js').default
+exports.isMac = require('./built/utils/is-mac.js').default
+exports.setSelectionRange = require('./built/utils/set-selection-range.js').default
+exports.updateContent = require('./built/utils/update-content.js').default
