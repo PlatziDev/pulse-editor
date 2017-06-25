1034fa3d146bc9e3f0ba66fbe669c0f5c5a81b19
Sergio Daniel Xalambrí <sergiodxa@gmail.com>
2017-06-25 04:14:14 -0500

    Export EmojiItem and EmojiCode components

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ exports.ButtonBar = require('./built/components/button-bar.js').default
 exports.ButtonGroup = require('./built/components/button-group.js').default
 exports.Editor = require('./built/components/editor.js').default
 exports.EmojiBar = require('./built/components/emoji-bar.js').default
+exports.EmojiCode = require('./built/components/emoji-code.js').default
+exports.EmojiItem = require('./built/components/emoji-item.js').default
 exports.Field = require('./built/components/field.js').default
 exports.Preview = require('./built/components/preview.js').default
